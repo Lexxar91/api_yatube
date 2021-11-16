6752125fd74011f626c312312712272119e53c12
Дмитрий Муратов <myratov91@yandex.ru>
2021-11-16 10:10:36 +0300

    homework3

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -21,13 +21,12 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment
     serializer_class = CommentSerializer
     permission_classes = (OwnerOrReadOnly, IsAuthenticated)
 
     def get_queryset(self):
         post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
-        return post.comments
+        return post.comments.all()
 
     def perform_create(self, serializer):
         post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
