3d03fe2989452cc96a244ca44512a1ad4f2191a4
Дмитрий Муратов <myratov91@yandex.ru>
2021-11-16 10:14:37 +0300

    check pls

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from posts.models import Post, Group, Comment
+from posts.models import Post, Group
 from .serializers import PostSerializer, GroupSerializer, CommentSerializer
 from .permissions import OwnerOrReadOnly
 
